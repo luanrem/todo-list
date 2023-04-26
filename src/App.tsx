2b24193b340d8d0c1ee68532cdfaef0757f5761d
luanrem <luanrem@gmail.com>
2023-04-26 20:37:32 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import styles from './App.module.css'
+
+import './global.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>hello world</p>
+    </>
+  )
+}
+
+export default App
