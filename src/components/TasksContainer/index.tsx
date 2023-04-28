11123e3634b693a767097efdebd4792579332475
luanrem <luanrem@gmail.com>
2023-04-27 22:20:42 -0300

    fix: change the number padding

--- src/components/TasksContainer/index.tsx
+++ src/components/TasksContainer/index.tsx
@@ -1,8 +1,14 @@
 import styles from './TaskContainer.module.css'
 
 import clipboard from '../../assets/Clipboard.svg'
+import { useState } from 'react'
+import { TaskCard } from '../TaskCard'
 
 export function TasksContainer() {
+  const [hasTask, setHasTask] = useState(false)
+
+  const tasks = [1, 2, 3, 4]
+
   return (
     <div className={styles.container}>
       <header className={styles.tasksHeader}>
@@ -12,15 +18,22 @@ export function TasksContainer() {
         </div>
         <div>
           <strong>Concluídas</strong>
-          <p>2</p>
+          <p>2 de 5</p>
         </div>
       </header>
       <div className={styles.content}>
-        <div className={styles.emptyTaskBoard}>
-          <img src={clipboard} alt="ClipBoard" />
-          <strong>Você ainda não tem tarefas cadastradas</strong>
-          <p>Crie tarefas e organize seus itens a fazer</p>
-        </div>
+        {hasTask ?
+
+          <div className={styles.emptyTaskBoard}>
+            <img src={clipboard} alt="ClipBoard" />
+            <strong>Você ainda não tem tarefas cadastradas</strong>
+            <p>Crie tarefas e organize seus itens a fazer</p>
+          </div> :
+          tasks.map(element => {
+            return <TaskCard key={element} />
+          })
+        }
+
       </div>
     </div>
   )
