ab4d4f86f9b613319ca8a419c5339fb528b71fdd
luanrem <luanrem@gmail.com>
2023-04-28 23:19:32 -0300

    feat: implement TaskCard

--- src/components/TaskCard/index.tsx
+++ src/components/TaskCard/index.tsx
@@ -1,5 +1,16 @@
+import { Trash } from '@phosphor-icons/react'
+
+import styles from './TaskCard.module.css'
+
 export function TaskCard() {
   return (
-    <h1>Test</h1>
+    <div className={styles.container}>
+      <div>
+        <input type="checkbox" />
+        <label htmlFor="lang1">Java</label>
+      </div>
+      <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+      <Trash />
+    </div>
   )
 }
