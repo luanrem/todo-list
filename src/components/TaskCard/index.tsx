eb1eae487fececbaf5ce61ee80741a2ad5c752cd
luanrem <luanrem@gmail.com>
2023-04-29 14:43:05 -0300

    fix: change styles of checkbox

--- src/components/TaskCard/index.tsx
+++ src/components/TaskCard/index.tsx
@@ -1,14 +1,20 @@
-import { Trash } from '@phosphor-icons/react'
+import { Trash, Check } from '@phosphor-icons/react'
 
 import styles from './TaskCard.module.css'
+import { useState } from 'react'
 
 export function TaskCard() {
+  const [checked, setChecked] = useState(true)
+
+  function handleCheck() {
+    setChecked(!checked)
+  }
+
   return (
     <div className={styles.container}>
-      <div>
-        <input type="checkbox" />
-        <label htmlFor="lang1">Java</label>
-      </div>
+      <button onClick={handleCheck} className={`${styles.checkboxField} ${checked && styles.checked}`}>
+        {checked ? <Check size={13} weight='bold' /> : ''}
+      </button>
       <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
       <Trash />
     </div>
