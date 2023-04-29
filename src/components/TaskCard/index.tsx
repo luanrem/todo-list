4fbe08c4491c8dafb59c29aab1f6612c6152f8db
luanrem <luanrem@gmail.com>
2023-04-29 14:48:07 -0300

    feat: style trash button

--- src/components/TaskCard/index.tsx
+++ src/components/TaskCard/index.tsx
@@ -15,8 +15,10 @@ export function TaskCard() {
       <button onClick={handleCheck} className={`${styles.checkboxField} ${checked && styles.checked}`}>
         {checked ? <Check size={13} weight='bold' /> : ''}
       </button>
-      <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
-      <Trash />
+      <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed </p>
+      <button className={styles.trashButton}>
+        <Trash size={20} />
+      </button>
     </div>
   )
 }
